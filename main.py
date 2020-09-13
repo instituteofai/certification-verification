5afc9ddc378fddbfdd3ac9f9bf4c90e2a3130c2e
Chandan Kumar <chandankumar99341@gmail.com>
2020-09-13 13:59:25 +0530

    WIP: certificate page

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
-from starlette.routing import Route
+from starlette.routing import Route, Mount
 from verifier import handle_verification
+from starlette.staticfiles import StaticFiles
 
 # TODO return a form where credential GUID can be pasted for verification
 async def homepage(request):
@@ -12,4 +13,8 @@ async def verify(request):
     return await handle_verification(request)
 
 
-app = Starlette(routes=[Route('/', homepage), Route('/verify/{guid}', verify)])
+app = Starlette(
+    routes=[
+        Route('/', homepage), Route('/verify/{guid}', verify),
+        Mount('/static', app=StaticFiles(directory='static'), name="static")
+    ])
